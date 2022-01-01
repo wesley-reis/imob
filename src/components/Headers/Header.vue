3bb7bf1140ee4e902a331a5078dfbb84eb36319a
Wesley Rodrigues dos Reis <69157081+wesley-reis@users.noreply.github.com>
2022-01-01 10:30:14 -0300

    Finalizando ajustes do Menu

--- src/components/Headers/Header.vue
+++ src/components/Headers/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="
+      z-10
       fixed
       bg-blue-1000
       w-full
@@ -72,21 +73,73 @@
       contato@minhaimob.com.br
     </p>
   </div>
-  <div class="pt-8 px-24 flex justify-between items-center shadow-md overflow-hidden">
-      <img src="../../assets/images/logo.png" alt="imobiliaria" class="w-1/4">
-      <div class="flex justify-end items-center gap-6 py-8 text-base font-extralight">
-          <a class="text-blue-600" href="">Home</a>
-          <a href="">Destaque</a>
-          <a href="">Alugar</a>
-          <a href="">Compar</a>
-          <a href="">Contato</a>
+
+  <nav class="bg-white pt-10">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+      <img src="../../assets/images/logo.png" alt="imobiliaria" class="w-2/6 md:w-1/5">
+
+      <div @click="showMenu = !showMenu" class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+        <!-- Mobile menu button-->
+        <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-blue-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+        
+          <svg v-if="!showMenu" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+         
+          <svg v-if="showMenu" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
       </div>
+
+      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
+
+        <div class="hidden sm:block sm:ml-6">
+          <div class="flex space-x-4 text-base font-extralight">
+            <a href="#" class="text-blue-600 px-3 py-2 rounded-md" aria-current="page">Home</a>
+
+            <a href="#" class="hover:text-blue-600 px-3 py-2 rounded-md">Destaque</a>
+
+            <a href="#" class="hover:text-blue-600 px-3 py-2 rounded-md">Alugar</a>
+
+            <a href="#" class="hover:text-blue-600 px-3 py-2 rounded-md">Comprar</a>
+            
+            <a href="#" class="hover:text-blue-600 px-3 py-2 rounded-md">Contato</a>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div :class="showMenu ? 'flex' : 'hidden'" class="sm:hidden" id="mobile-menu">
+    <div class="px-2 pt-2 pb-3 space-y-1 text-base font-extralight">
+      <a href="#" class="text-blue-600 block px-3 py-2 rounded-md" aria-current="page">Home</a>
+
+      <a href="#" class="hover:text-blue-600 block px-3 py-2 rounded-md">Destaque</a>
+
+      <a href="#" class="hover:text-blue-600 block px-3 py-2 rounded-md">Alugar</a>
+
+      <a href="#" class="hover:text-blue-600 block px-3 py-2 rounded-md">Comprar</a>
+
+      <a href="#" class="hover:text-blue-600 block px-3 py-2 rounded-md">Contato</a>
+
+    </div>
+  </div>
+  </nav>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data(){
+    return{
+      showMenu: false,
+    }
+  }
 };
 </script>
 
