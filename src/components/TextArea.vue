e5258f79531af1b5cb833587099ec114fcd8a5dd
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-01-05 16:34:17 -0300

    inclusão de formulario contact

---
+++ src/components/TextArea.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div v-if="label" class="flex justify-start items-center gap-1">
+    <slot/>
+    <label class="text-base font-medium text-gray-300" :for="name">{{label}}</label>
+  </div>
+  <div class="bg-gray-50 rounded-md border-b border-gray-300 w-full overflow-hidden">
+    <textarea :placeholder="placeholder" rows="5" :type="type"  :name="name" :id="name" class="w-full py-2 px-3 bg-transparent outline-none text-gray-800"/>
+  </div>
+  <span v-if="error" class="text-xs text-red-600">campo {{ name }} é obrigatório</span>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:'Input',
+    props:{
+        label:String,
+        placeholder:String,
+        type:String,
+        name:String,
+        error:String,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/web/Contact.vue
+++ src/views/web/Contact.vue
@@ -1,15 +1,83 @@
 <template>
-  <div>
-  <h1 class="text-4xl text-center font-semibold" >Conato</h1>
+  <div class="container mx-auto py-12 px-5 sm:px-14 md:px-20">
+    <div
+      class="
+        flex flex-col
+        justify-center
+        items-center
+        text-gray-800 text-center
+      "
+    >
+      <h1 class="text-4xl text-center font-semibold text-sky-700 text-center">
+        Entre em Contato Conosco
+      </h1>
+      <p>
+        Quer conversar com um corretor exclusivo e ter o atendimento
+        diferenciado em busca do seu imóvel dos sonhos?
+      </p>
+      <p>
+        Preencha o formulário abaixo e vamos lhe direcionar para alguém que
+        entende a sua necessidade!
+      </p>
+    </div>
+
+    <div class="w-full px-6 py-7 bg-gray-200 mt-10">
+      <p>Envie um e-mail</p>
+      <form class="flex flex-col gap-3 w-full mt-4">
+        <InputWhite
+          :type="'string'"
+          :name="'name'"
+          :placeholder="'Digite seu nome'"
+          :error="'name'"
+        />
+
+        <InputWhite
+          :type="'email'"
+          :name="'email'"
+          :placeholder="'Digite seu melhor e-mail'"
+          :error="'email'"
+        />
+
+        <TextArea
+          :type="'message'"
+          :name="'message'"
+          :placeholder="'Escreva sua mensagem...'"
+          :error="'message'"
+        />
+
+        <div class="flex justify-end">
+          <button
+            type="submit"
+            class="
+              bg-sky-700
+              w-32
+              rounded-md
+              p-3
+              font-normal
+              text-sm text-white
+              hover:opacity-90
+              flex
+              justify-center
+              item-center
+            "
+          >
+            Enviar Contato
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import InputWhite from "../../components/InputWhite.vue";
+import TextArea from "../../components/TextArea.vue";
 
 export default {
-  name: 'Contact',
+  name: "Contact",
   components: {
-  }
-}
+    InputWhite,
+    TextArea,
+  },
+};
 </script>
