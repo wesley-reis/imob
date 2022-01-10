ef6e5e5dcfd5c79d272cd354e69d2320e8a03e52
Wesley Rodrigues dos Reis <69157081+wesley-reis@users.noreply.github.com>
2022-01-10 00:06:44 -0300

    instalando keen-slider

--- src/views/web/Propertie.vue
+++ src/views/web/Propertie.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    <h1 class="text-4xl text-center font-semibold" >Propertie</h1>
+  <div class="container mt-5 px-4 py-10">
+    <h1 class="text-4xl text-sky-700">
+      Linda Casa no Joa com vista para a Lagoa Central
+    </h1>
+    <p class="text-gray-600 text-sm font-medium mt-2">
+      Imóvel Residencial - Casa Joa
+    </p>
+
+    <div>
+      <slider/>
+    </div>
+    <p class="text-gray-500 text-sm font-normal">
+      IPTU: R$ 100,00 | Condomínio: R$ 350,00
+    </p>
+    <p class="text-gray-500 text-2xl font-normal">
+      Valor do Aluguel: R$ 1.200,00
+    </p>
   </div>
 </template>
 
 <script>
+import Slider from '../../components/Slider/Slider.vue';
 export default {
-    name:"Propertie",
-}
+  components: { Slider },
+  name: "Propertie",
+
+  
+};
 </script>
 
 <style>
-
 </style>
